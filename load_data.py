522f64229b1b18c0ccadcdfa0dca32abb72f7d6a
Allison Chau <allison.chau@annalect.com>
2019-09-17 14:45:37 -0400

    added pseudocode

--- load_data.py
+++ load_data.py
@@ -16,15 +16,19 @@ from collections import Counter
 
 # import os
 
-# order_dict={}
-
+# Declaring global dicts to be updated throughout file iteration
 user_dict={}
 line_item_dict={}
 
+
+# Variables to include and exclude for each dict
 order_exclude_list=['line_items','id']
 user_include_list=['customer_locale','buyer_accepts_marketing','contact_email','phone']
+
+# String variables to convert to datetime
 timestamp_vars=['created_at','processed_at','updated_at','closed_at','cancelled_at']
 
+# Declaring SQLalchemy data types
 order_type_dict={
     'email':String,
     'number':BIGINT,
@@ -44,7 +48,7 @@ order_type_dict={
 }
 
 def download_extract_zip(url):
-    # print('hello')
+    # Function to load data from url and return fn, fobj as iterables
     r=requests.get(url)
     if (r.status_code==200):
         print('Loading data...')
@@ -57,30 +61,41 @@ def download_extract_zip(url):
                 yield zipinfo.filename, thefile
 
 def load_order_row(orderrows):
+    # Function to load and return order data in a daily file
+    # Also, to update global variables line_item_dict and order_dict
+    # Probably a better way to do this
+
     order_dict={}
     
     for orderrow in orderrows:
+        # Declare useful vars
         order_id=orderrow['id']
         user_id=orderrow['user_id']
-        line_item_list=orderrow['line_items']
-        line_item_total=len(line_item_list)
         processed_at=convert_timestring_utc(orderrow['processed_at'])
         total_price=float(orderrow['total_price'])
+
+        # Separate out line item array
+        line_item_list=orderrow['line_items']
+        line_item_total=len(line_item_list)
         # item_total=sum([x['quantity'] for x in line_item_list])
 
+        # Update order dict
         order_dict[order_id]={k:v for k,v in orderrow.items() 
                                 if( k not in set(order_exclude_list) 
                                 and k not in set(user_include_list))}
         order_dict[order_id]['line_item_total']=line_item_total
 
+        # Update line item dict
         for line_item in line_item_list:
             line_item_id=line_item['id']
             line_item_dict[line_item_id]={k:v for k,v in line_item.items() if k!='id'}
             line_item_dict[line_item_id]['order_id']=order_id
 
+        # Update user dict
         if (user_id not in user_dict):
             user_dict[user_id]={k:v for k, v in orderrow.items() 
                                 if k in set(user_include_list)}
+            # Keep track of total orders and spend for future bucketing
             user_dict[user_id]['total_orders']=1
             user_dict[user_id]['total_spend']=total_price
             user_dict[user_id]['min_processed_at']=processed_at
@@ -91,6 +106,7 @@ def load_order_row(orderrows):
             prev_max=user_dict[user_id]['max_processed_at']
             min_processed_at=min(prev_min,processed_at)
             max_processed_at=max(prev_max,processed_at)
+            # Update user vars with most recent ones
             if max_processed_at!=prev_max:
                 for (k, v) in orderrow.items():
                     if k in set(user_include_list):
@@ -99,15 +115,15 @@ def load_order_row(orderrows):
             user_dict[user_id]['total_spend']+=total_price
             user_dict[user_id]['min_processed_at']=min_processed_at
             user_dict[user_id]['max_processed_at']=max_processed_at
+            # Keep track of total days we have seen user
             user_dict[user_id]['total_days']=(max_processed_at-min_processed_at).days+1
 
-
-    order_df=pd.DataFrame.from_dict(order_dict,orient='index')
-        
+    order_df=pd.DataFrame.from_dict(order_dict,orient='index')   
     return order_df
 
 
 def convert_timestring_utc(x):
+    # Given string, convert into standard UTC datetime object
     if x is None:
         return x
     x=x[:-6]+''.join(x[-6:].split(':'))
@@ -116,12 +132,14 @@ def convert_timestring_utc(x):
 
 
 def convert_to_datetime(df):
+    # Convert all appropriate columns to datetime
     for var in timestamp_vars:
         if var in df:
             df[var]=df[var].map(convert_timestring_utc)
     return df
 
 def append_df_to_psql(df,table_name,engine):
+    # Add dataframe to PSQL table
     df=convert_to_datetime(df)
     df.to_sql(name=table_name, con=engine, if_exists = 'append', index=True,dtype=order_type_dict)
 
@@ -133,19 +151,21 @@ def main(args):
     engine = create_engine('postgresql://'+creds.username+':'
                            +creds.password+'@'+creds.host+':'
                            +creds.port+'/'+creds.db,echo=False)
-
+    # Declare fn_count and month to track load status
     fn_count=0
     month=''
+    # Iterate through the extracted files
     for (fn,fobj) in datafiles:
         daily_order_data=json.load(fobj)['orders']
         order_df=load_order_row(daily_order_data)
+        # Extract year and month from filename for tracking
+        # and for table naming
         fn_month=fn[:4]+'_'+fn[5:7]
         table_name='orders__%s'%fn_month
-
         append_df_to_psql(order_df,table_name,engine)
 
-        
-        
+        # Logic for printing out load status
+        # Print out how many files were inserted into a monthly table
         if (fn_count==0):
             month=fn_month
             fn_count+=1
@@ -158,12 +178,16 @@ def main(args):
             month=fn_month
             fn_count=1
             break
+    # Print out final insert statement
     print('Loaded %d file(s) into %s'%(fn_count,'orders__%s'%month))
+    
+    # Add in line item and user stat tables
     line_item_df=pd.DataFrame.from_dict(line_item_dict,orient='index')
     append_df_to_psql(line_item_df,'order_line_item_mapping',engine)
     print('Loaded %d record(s) into %s'%(len(line_item_df),'order_line_item_mapping'))
     user_df=pd.DataFrame.from_dict(user_dict,orient='index')
     append_df_to_psql(user_df,'user_stats',engine)
     print('Loaded %d record(s) into %s'%(len(user_df),'user_stats'))
+
 if __name__ == "__main__":
     main(sys.argv)
